4aa32acafebf5057fc00daa158e2e9e9fd5f3fb4
terricored <terricored@gmail.com>
2026-03-30 14:36:48 +0300

    Small touches

--- data.js
+++ data.js
@@ -1,5 +1,19 @@
 const artData = [
   {
+    "title": "бестіаріат",
+    "filename": "test.jpg",
+    "desc": "Capturing the intensity of a dying star.",
+    "year": "2025",
+    "medium": "Oil on Canvas"
+  },
+  {
+    "title": "уродікі",
+    "filename": "test.jpg",
+    "desc": "Capturing the intensity of a dying star.",
+    "year": "2025",
+    "medium": "Oil on Canvas"
+  },
+  {
     "title": "про мене",
     "filename": "test.gif",
     "desc": "Текст для перевірки шрифту. Чуєш їх, доцю, га? Кумедна ж ти, айбо! Босхоз мовчить — і враз ґуля.",
@@ -21,6 +35,13 @@ const artData = [
     "medium": "Digital Animation"
   },
   {
+    "title": "Екстра",
+    "filename": "test.jpg",
+    "desc": "Capturing the intensity of a dying star.",
+    "year": "2025",
+    "medium": "Oil on Canvas"
+  },
+  {
     "title": "ВПО",
     "filename": "test.jpg",
     "desc": "Capturing the intensity of a dying star.",
@@ -29,20 +50,6 @@ const artData = [
   },
   {
     "title": "зіни",
-    "filename": "test.jpg",
-    "desc": "Capturing the intensity of a dying star.",
-    "year": "2025",
-    "medium": "Oil on Canvas"
-  },
-  {
-    "title": "бестіаріат",
-    "filename": "test.jpg",
-    "desc": "Capturing the intensity of a dying star.",
-    "year": "2025",
-    "medium": "Oil on Canvas"
-  },
-  {
-    "title": "уродікі",
     "filename": "test.jpg",
     "desc": "Capturing the intensity of a dying star.",
     "year": "2025",
